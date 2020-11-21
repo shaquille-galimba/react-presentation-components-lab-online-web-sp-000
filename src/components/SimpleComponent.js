328fbde6845dacedb103c594512d562c1e7d05a5
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-11-21 10:34:15 -0500

    commit tree 43c4bf54fcf2b0114ed7c52c726122841ede5e24

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			mood: "happy"
+		}
+	}
+
+	handleClick = () => {
+		this.setState(previousState => {
+			return {
+				mood: previousState.mood === "happy" ? "sad" : "happy"
+			}
+		})
+	}
+
+	render() {
+		return (
+			<div onClick={this.handleClick}>{this.state.mood}</div>
+		)
+	}
+}
+
+export default SimpleComponent
